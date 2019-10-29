69e84fcfc2adb7afbeed5f1e9f5e223a19e0f0a8
Zoe Henderson <zzhenders@gmail.com>
2019-10-29 12:56:22 -0700

    Minimal server.py

--- server/server.py
+++ server/server.py
@@ -1,1 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+
+DIRECTORY = os.path.dirname(__file__)
+CLIENT = DIRECTORY + "/../client/build"
+
+app = Flask(
+    __name__, static_folder=f"{CLIENT}/static", template_folder=f"{CLIENT}"
+)
+
+# Serve React
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
